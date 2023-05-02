4dbc7f3a7a56384a3d6358d68aa4b18af4527e5d
Ep1cType <DenRus38@hotmail.com>
2023-05-03 01:00:22 +0700

    feat: login by email flow

---
+++ src/shared/api/auth/request.ts
@@ -0,0 +1,11 @@
+import { createEffect } from 'effector';
+import { SignInError, SignInPayload, SignInResponse } from 'shared/api/auth/types';
+import { AuthApi } from 'shared/api/auth/index';
+
+const Api = new AuthApi();
+
+export const loginByEmailFx = createEffect<SignInPayload, SignInResponse, SignInError>(async (form) => {
+	const response = await Api.signIn(form);
+
+	return response.data;
+});
