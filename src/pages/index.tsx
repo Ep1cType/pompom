2039775b94ca79d2ee3812d7191b2369b364f0c1
Ep1cType <DenRus38@hotmail.com>
2023-05-03 21:53:30 +0700

    fix: image size error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { CharacterApi } from 'shared/api/character';
+
+const Api = new CharacterApi();
 
 export default function HomePage() {
+	useEffect(() => {
+		Api.getCharacter({ name: "Astra", locale: "ru" });
+	}, [])
+
 	return (
 		<div className="container mx-auto px-4">
 			<h1>Welcome</h1>
