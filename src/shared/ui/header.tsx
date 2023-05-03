64d4711ed9b08455df1486ed1caf120fdbd9747a
Ep1cType <DenRus38@hotmail.com>
2023-05-04 03:18:25 +0700

    fix: header

--- src/shared/ui/header.tsx
+++ src/shared/ui/header.tsx
@@ -1,71 +1,81 @@
 import React from 'react';
 import Link from 'next/link';
+import clsx from 'clsx';
 
-export const Header = () => {
+type Props = {
+	className?: string;
+}
+
+export const Header = ({ className }: Props) => {
 	return (
-		<header className="navbar bg-blue-900">
-			<div className="navbar-start">
-				<div className="dropdown">
-					<label tabIndex={0} className="btn btn-ghost lg:hidden">
-						<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-					</label>
-					<ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+		<header className={clsx('navbar bg-blue-900', className)}>
+			<div className="container mx-auto px-4">
+				<div className='navbar-start'>
+					<div className='dropdown'>
+						<label tabIndex={0} className='btn btn-ghost lg:hidden'>
+							<svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5' fill='none' viewBox='0 0 24 24'
+									 stroke='currentColor'>
+								<path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h8m-8 6h16' />
+							</svg>
+						</label>
+						<ul tabIndex={0} className='menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 bg-blue-900 rounded-box w-52'>
+							{headerItems.map((item, index) => (
+								<li key={index}>
+									<Link href={item.link}>{item.title}</Link>
+								</li>
+							))}
+							{/*<li><a>Турниры</a></li>*/}
+							{/*<li tabIndex={0}>*/}
+							{/*	<a className="justify-between">*/}
+							{/*		Parent*/}
+							{/*		<svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>*/}
+							{/*	</a>*/}
+							{/*	<ul className="p-2">*/}
+							{/*		<li><a>Submenu 1</a></li>*/}
+							{/*		<li><a>Submenu 2</a></li>*/}
+							{/*	</ul>*/}
+							{/*</li>*/}
+							{/*<li><a>Спортсмены</a></li>*/}
+						</ul>
+					</div>
+					<Link href={'/'} className='btn btn-ghost normal-case text-xl'>HSR</Link>
+				</div>
+				<div className='navbar-center hidden lg:flex'>
+					<ul className='menu menu-horizontal px-1'>
 						{headerItems.map((item, index) => (
 							<li key={index}>
 								<Link href={item.link}>{item.title}</Link>
 							</li>
 						))}
-						{/*<li><a>Турниры</a></li>*/}
+						{/*<li><a>Item 1</a></li>*/}
 						{/*<li tabIndex={0}>*/}
-						{/*	<a className="justify-between">*/}
+						{/*	<a>*/}
 						{/*		Parent*/}
-						{/*		<svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>*/}
+						{/*		<svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>*/}
 						{/*	</a>*/}
 						{/*	<ul className="p-2">*/}
 						{/*		<li><a>Submenu 1</a></li>*/}
 						{/*		<li><a>Submenu 2</a></li>*/}
 						{/*	</ul>*/}
 						{/*</li>*/}
-						{/*<li><a>Спортсмены</a></li>*/}
+						{/*<li><a>Item 3</a></li>*/}
 					</ul>
 				</div>
-				<Link href={"/"} className="btn btn-ghost normal-case text-xl">HSR</Link>
+				{/*<div className="navbar-end">*/}
+				{/*	<label htmlFor="auth-modal" className="btn">Login</label>*/}
+				{/*</div>*/}
 			</div>
-			<div className="navbar-center hidden lg:flex">
-				<ul className="menu menu-horizontal px-1">
-					{headerItems.map((item, index) => (
-						<li key={index}>
-							<Link href={item.link}>{item.title}</Link>
-						</li>
-					))}
-					{/*<li><a>Item 1</a></li>*/}
-					{/*<li tabIndex={0}>*/}
-					{/*	<a>*/}
-					{/*		Parent*/}
-					{/*		<svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>*/}
-					{/*	</a>*/}
-					{/*	<ul className="p-2">*/}
-					{/*		<li><a>Submenu 1</a></li>*/}
-					{/*		<li><a>Submenu 2</a></li>*/}
-					{/*	</ul>*/}
-					{/*</li>*/}
-					{/*<li><a>Item 3</a></li>*/}
-				</ul>
-			</div>
-			{/*<div className="navbar-end">*/}
-			{/*	<label htmlFor="auth-modal" className="btn">Login</label>*/}
-			{/*</div>*/}
 		</header>
 	);
 };
 
 const headerItems = [
 	{
-		title: "Персонажи",
-		link: "/characters"
+		title: 'Персонажи',
+		link: '/characters',
 	},
 	{
-		title: "Тир-лист",
-		link: "/tier-list"
-	}
-]
+		title: 'Тир-лист',
+		link: '/tier-list',
+	},
+];
--- src/shared/ui/layout.tsx
+++ src/shared/ui/layout.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from 'react';
 import { Header } from 'shared/ui/header';
 import { AuthModal } from 'widgets/auth-modal/ui';
 import clsx from 'clsx';
+import { Footer } from 'shared/ui/footer';
 
 type Props = {
 	children: ReactNode;
@@ -10,13 +11,11 @@ type Props = {
 export const Layout = ({ children, className }: Props) => {
 	return (
 		<div className='flex flex-col min-h-screen h-full text-white w-full'>
-			<Header />
+			<Header className={className} />
 			<main className={clsx('flex-grow bg-blue-950', className)}>
 				{children}
 			</main>
-			<footer className='flex-shrink-0 '>
-				hello
-			</footer>
+			<Footer className={className} />
 			{/*<AuthModal />*/}
 		</div>
 	);
@@ -26,4 +25,4 @@ export const variants = {
 	hidden: { opacity: 0, x: -200, y: 0 },
 	enter: { opacity: 1, x: 0, y: 0 },
 	exit: { opacity: 0, x: 0, y: -100 },
-}
+};
