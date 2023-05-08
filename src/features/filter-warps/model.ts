e9980dd86a4c83ca4c48a6ddff3ad306041499e1
Ep1cType <DenRus38@hotmail.com>
2023-05-09 00:27:43 +0700

    feat: remove header.

--- src/features/filter-warps/model.ts
+++ src/features/filter-warps/model.ts
@@ -3,6 +3,6 @@ import { GachaTypeList } from 'shared/api/warp/types';
 
 export const setWarpFilter = createEvent<GachaTypeList>();
 
-export const $warpFilter = createStore<GachaTypeList>('1');
+export const $warpFilter = createStore<GachaTypeList>('2');
 
 $warpFilter.on(setWarpFilter, (_, filter) => filter);
