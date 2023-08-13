3120d0c71fdcea871ac6876afe5da5d824f382d3
denischernykh <DenRus38@hotmail.com>
2023-08-13 15:29:21 +0800

    feat: prefetch false on main page

--- src/molecules/navigation-card/navigation-card.tsx
+++ src/molecules/navigation-card/navigation-card.tsx
@@ -9,8 +9,8 @@ type Props = {
 
 export const NavigationCard = ({ title, children, link }: Props) => {
   return (
-    <div className="rounded-2xl bg-blue-900 shadow-blue-900 shadow-info hover:opacity-70">
-      <Link className="block p-6 md:p-8" href={link}>
+    <div className="rounded-2xl bg-blue-900 shadow-info hover:opacity-70">
+      <Link className="block p-6 md:p-8" prefetch={false} href={link}>
         <h2 className="mb-2 text-2xl md:mb-4">{title}</h2>
         {children}
         <span className="mt-2 block underline">Перейти</span>
