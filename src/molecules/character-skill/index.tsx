fa6d402640a466ed3092aca6d72ebeaa90ba85e0
denischernykh <DenRus38@hotmail.com>
2023-10-10 01:56:35 +0800

    feat: smooth range input

--- src/molecules/character-skill/index.tsx
+++ src/molecules/character-skill/index.tsx
@@ -12,23 +12,26 @@ export const CharacterSkill = ({ skill }: Props) => {
   const maxSkillLevel = skillLevelList.length - 1;
 
   return (
-    <div className="flex flex-col rounded-2xl bg-blue-900 p-4">
+    <div className="flex flex-col rounded-2xl bg-blue-900 p-4 shadow-blue-900 drop-shadow-md">
       <h3 className="mb-1 text-base font-bold">{skill.name}</h3>
       <p className="mb-2 text-sm font-medium text-orange">{skill.type}</p>
       <p
         className="flex-grow text-base/snug [&>[data-tooltip-id='tooltip-skill']]:cursor-pointer [&>[data-tooltip-id='tooltip-skill']]:underline [&>span]:font-bold"
-        dangerouslySetInnerHTML={{ __html: skillLevelList[initialSkill] }}
+        dangerouslySetInnerHTML={{
+          __html: skillLevelList[Math.round(initialSkill)],
+        }}
       />
       {skillLevelList.length > 1 && (
         <div className="mt-4">
-          <span className="mr-2">Уровень: {initialSkill + 1}</span>
+          <span className="mr-2">Уровень: {Math.round(initialSkill) + 1}</span>
           <p className="inline space-x-1">
             <input
-              className="range range-accent range-xs"
+              className="range-white range range-xs"
               type="range"
               min={0}
               max={maxSkillLevel}
               value={initialSkill}
+              step={0.0001}
               onChange={(e) => setInitialSkill(Number(e.target.value))}
             />
           </p>
